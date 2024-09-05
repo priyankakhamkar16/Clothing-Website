90c5f401b9c0197e3157b892f8935e2b42e73e4f
Priyanka khamkar <118741221+priyankakhamkar16@users.noreply.github.com>
2024-09-05 11:48:19 +0530

    Update Signup.js

--- clothing-website/src/components/Signup.js
+++ clothing-website/src/components/Signup.js
@@ -21,7 +21,7 @@ function Signup() {
     const signupData = { name, email, username, password };
 
     try {
-      const response = await fetch('https://clothing-website-gw7l.vercel.app/api/auth/register', {
+      const response = await fetch('https://clothing-website-3nju.vercel.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
